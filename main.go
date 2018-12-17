6a8c3b9baa46b2c7111130104b568ada55542a57
maseology <maseology@pm.me>
2018-12-17 10:57:12 -0500

    add MFP option

--- main.go
+++ main.go
@@ -27,16 +27,26 @@ func run(p Profile, simLength, maxTimeStep, initSe float64) {
 	time := 0.0
 	sumInfiltration := 0.0
 	totiter := 0
+	solver := 3 // 1: cell centered finite volume; 2: todo; 3: matrix flux potentential Newton Rapson
 	// boundary conditions
 	ubPotential := p.P[1].He
 	isFreeDrainage := true
 	// state variables
 	var t ProfileState
-	t.InitializeWater(p, initSe)
+	var sol func(float64, float64, bool) (bool, int, float64)
+	t.InitializeWater(p, initSe, solver)
+	switch solver {
+	case 1:
+		sol = t.CellCentFiniteVolWater
+	case 3:
+		sol = t.NewtonRapsonMFP
+	default:
+		panic("solver type unavailable")
+	}
 
 	for time < endTime {
 		dt = math.Min(dt, endTime-time)
-		ok, inneriter, infil := t.CellCentFiniteVolWater(p, dt, ubPotential, isFreeDrainage)
+		ok, inneriter, infil := sol(dt, ubPotential, isFreeDrainage)
 		totiter += inneriter
 		if ok {
 			for i := 0; i <= NsubLay+1; i++ {
@@ -54,7 +64,11 @@ func run(p Profile, simLength, maxTimeStep, initSe float64) {
 			dt = math.Max(dt/2.0, 1.0)
 			for i := 0; i <= NsubLay+1; i++ {
 				t.T[i] = t.Tl[i]
-				t.Psi[i] = t.PM[i].GetPsi(t.T[i])
+				if solver == 3 {
+					t.Psi[i] = t.PM[i].MFPfromTheta(t.T[i])
+				} else {
+					t.Psi[i] = t.PM[i].GetPsi(t.T[i])
+				}
 			}
 		}
 	}
